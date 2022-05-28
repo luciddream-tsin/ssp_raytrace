f26faf18121bd511edcbc7f689d623624e0f5b56
tsin <chunfeng_tsin@163.com>
2022-05-28 14:33:07 +0800

    抗锯齿， add camera.

--- Main.cpp
+++ Main.cpp
@@ -9,55 +9,48 @@
 #include "hitable.h"
 #include "hitable_list.h"
 #include "sphere.h"
+#include "camera.h"
+#include "random.h"
 
 
 vec3 color(const ray& r, hitable_list &world){
     hit_record record;
     if (world.hit(r, 0.0, FLT_MAX, record)){
-        return (record.normal+1.0)*0.5;
+        auto c = (record.normal+1.0)*0.5;
+        return c;
     }
-    vec3 unit_dir = make_unit(r.direc());
-    float t = (unit_dir.y()+1)*0.5;
+    float t = (r.direc().y()+1)*0.5;
     return vec3{1.0, 1.0, 1.0}*(1.0-t) + vec3{0.5, 0.7, 1.0}*t;
 }
 
 int main(){
     int nx = 200;
     int ny = 100;
-
     std::ofstream of("background.ppm");
     of << "P3" << std::endl << nx << " " << ny << std::endl << "255" << "\n";
-
-    vec3 left_lower_corner{-2.0, -1.0, -1.0};
-    vec3 horizontal{4.0, 0, 0};
-    vec3 vertical{0, 2.0, 0};
-    vec3 origin{0, 0, 0};
-
+    float sample_freq = 10;
     hitable* list[2];
     list[0] = new sphere(vec3{0, 0, -1}, 0.5);
     list[1] = new sphere(vec3{0, -100.5, -1}, 100.);
 
     hitable_list world{list, 2};
-
+    camera came{};
 
     for (int j = ny-1; j >=0 ; --j) {
         for (int i = 0; i < nx; ++i) {
 
-            float u = float(i)/float(nx);
-            float v = float(j)/float(ny);
-            vec3 b = (left_lower_corner + u * horizontal + v*vertical);
-            b.make_unit_vector();
-            const vec3 vvv =vec3{0,0,0};
-            b+= vvv;
-            ray r{origin, b};
-
-            vec3 c = color(r, world);
-
-            int cr = (int)(c[0] * 255);
-            int cg = (int)(c[1] * 255);
-            int cb = (int)(c[2] * 255);
-
-
+            ray r;
+            vec3 c{0,0,0};
+            for (int k = 0; k < sample_freq; ++k) {
+                float u = (float(i) + get_random())/float(nx);
+                float v = (float(j) + get_random())/float(ny);
+                r = came.get_ray(u, v);
+                c += color(r, world);
+            }
+            c /= sample_freq;
+            int cr = (int)(c[0] * 255.99);
+            int cg = (int)(c[1] * 255.99);
+            int cb = (int)(c[2] * 255.99);
             of << cr << " " << cg << " " << cb << std::endl;
         }
     }
---
+++ camera.h
@@ -0,0 +1,32 @@
+//
+// Created by tsin on 2022/5/28.
+//
+
+#ifndef RAYTRACE_WEEK1_CAMERA_H
+#define RAYTRACE_WEEK1_CAMERA_H
+#include "vec3.h"
+#include "random"
+#include "ray.h"
+
+class camera {
+public:
+    camera(){
+        left_lower_corner = vec3{-2.0, -1.0, -1.0};
+        horizontal = vec3{4.0, 0, 0};
+        vertical = vec3{0, 2.0, 0};
+        origin = vec3{0, 0, 0};
+    }
+    ray get_ray(float u, float v){
+        vec3 b = (left_lower_corner + u * horizontal + v*vertical);
+        b.make_unit_vector();
+        return ray{origin, b};
+    }
+
+    vec3 left_lower_corner{-2.0, -1.0, -1.0};
+    vec3 horizontal{4.0, 0, 0};
+    vec3 vertical{0, 2.0, 0};
+    vec3 origin{0, 0, 0};
+};
+
+
+#endif //RAYTRACE_WEEK1_CAMERA_H
